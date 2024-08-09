c492220c1d225626d60ca500dcf5be7c91c0ebfa
paulolima12 <hrpaulo1204@gmail.com>
2024-08-09 17:53:10 -0300

    certin

---
+++ 3BIMESTRE/etec-filmes/src/App.js
@@ -0,0 +1,22 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    
+    <>
+      <Header/>
+      <Banner/>
+      <Container/>
+      <h1>lorem ipsum</h1>
+      <p>lorem ipsum</p>
+      <Footer/>
+    </>
+  );
+
+  
+}
+
+export default App;
---
+++ 3BIMESTRE/etec-filmes/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            Desenvolvido por eo.paulinn &reg; 20k4
+        </footer>
+    );
+}
+
+export default Footer; 
