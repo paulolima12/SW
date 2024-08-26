1bd7ef9d696f4b7b7cc108e2540921d3de830e3b
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-08-26 07:57:26 -0300

    site arrumado

--- 3 BIMESTRE/php/site/cadastro_cliente.sql
+++ 3 BIMESTRE/php/site/cadastro_cliente.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 26/08/2024 às 06:16
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `cadastro_cliente`
@@ -60,7 +49,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'Paulo Augusto', 'emailteste@email.com', 'senha1234');
+(1, 'Plinis', 'emailteste@email.com', 'senhazeus1204');
 
 --
 -- Índices para tabelas despejadas
@@ -95,6 +84,3 @@ ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
